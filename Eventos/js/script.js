a536c4c4464ccb74cf06cc0c92c903ed1f07d123
RODOLFO GONCALVES DA SILVA <rodolfo.gsilva2@senacsp.edu.br>
2024-04-04 14:23:50 -0300

    aula04-04

--- Eventos/js/script.js
+++ Eventos/js/script.js
@@ -37,14 +37,59 @@ btnInsideContainer.addEventListener("click", (e) => {
   e.stopPropagation();
 });
 
-const a = document.querySelector("a");
+// const a = document.querySelector("a");
 
-a.addEventListener("click", (e) => {
-  e.preventDefault();
-  console.log("Não alterou a página");
-});
+// a.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   console.log("Não alterou a página");
+// });
 
 
-document.addEventListener("keyup", (e) => {
-  console.log(`Soltou a tecla ${e.key}`);
-});
+// document.addEventListener("keyup", (e) => {
+//   console.log(`Soltou a tecla ${e.key}`);
+// });
+// document.addEventListener("keydown", (e) => {
+//   console.log(`Pressionou a tecla ${e.key}`);
+// });
+
+
+// const mouseEvents = document.querySelector("#mouse");
+// mouseEvents.addEventListener("mousedown", () => {
+//   console.log("Pressionou botão");
+// });
+// mouseEvents.addEventListener("mouseup", () => {
+//   console.log("Soltou botão");
+// });
+// mouseEvents.addEventListener("dblclick", () => {
+//   console.log("Clique duplo");
+// });
+// window.addEventListener("scroll", (e) => {
+//   if (window.scrollY > 200) { 
+//     console.log("Passamos 200px!");
+//   }
+// });
+
+const dataAtual = new Date();
+console.log(dataAtual)
+
+// Obter o dia do mês
+const dia = dataAtual.getDate();
+console.log(`Dia: ${dia}`); // Saída: 4 (para 4 de abril de 2024)
+
+// Obter o mês (0-11, janeiro = 0)
+const mes = dataAtual.getMonth() + 1; // Adicionar 1 para obter o mês normal (1-12)
+console.log(`Mês: ${mes}`); // Saída: 4 (para abril)
+
+// Obter ano
+const ano = dataAtual.getFullYear(); // Obtem o ano completo
+console.log(`Mês: ${mes}`); // Saída: 2024
+
+// Obter a data completa formatada
+const dataFormatada = `${dia}/${mes}/${ano}`;
+console.log(`Data formatada: ${dataFormatada}`); // Saída: "04/04/2024
+
+const frutas = ["Maçã", "Banana", "Laranja", "Pera", "Uva"];
+
+// Encontrar a primeira fruta que começa com "L" usando `find()`
+const frutaComL = frutas.find(fruta => fruta.startsWith("L"));
+console.log(`Fruta com L: ${frutaComL}`); // Saída: "Laranja"
